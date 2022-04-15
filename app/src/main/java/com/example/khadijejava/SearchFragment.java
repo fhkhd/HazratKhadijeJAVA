8131dec7c9899d101caf9d7a553065e249307bf2
fhkhd <fhkhdkri@yahoo.com>
2022-04-15 14:17:03 +0430

    set items on search fragment

--- app/src/main/java/com/example/khadijejava/SearchFragment.java
+++ app/src/main/java/com/example/khadijejava/SearchFragment.java
@@ -1,64 +1,92 @@
 package com.example.khadijejava;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.khadijejava.api.ApiClient;
+import com.example.khadijejava.api.ApiService;
+import com.example.khadijejava.model.Data;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SearchFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SearchFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    LottieAnimationView lottieAnimationView;
+    RecyclerView recyclerView;
+    ApiService apiService;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search , container , false);
+        recyclerView = view.findViewById(R.id.recycler_search);
+        lottieAnimationView = view.findViewById(R.id.lottie_no_connection);
+        checkConnection();
+        return view;
     }
+
+    @SuppressLint("ResourceAsColor")
+    public void checkConnection(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if(wifi.isConnected()){
+            lottieAnimationView.setVisibility(View.INVISIBLE);
+            apiService = ApiClient.getClient().create(ApiService.class);
+            Call<Data> getApiPost = apiService.getPost();
+            getApiPost.enqueue(new Callback<Data>() {
+                @Override
+                public void onResponse(Call<Data> call, Response<Data> response) {
+//                Log.e("qqq", "onResponse: "+response.body().ok);
+                    recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+                    recyclerView.setAdapter(new SearchAdapter(response.body().result , getContext()));
+
+                }
+                @Override
+                public void onFailure(Call<Data> call, Throwable t) {
+                    Log.e("qqqq", "onFailure: ",t );
+
+                }
+            });
+
+        }else if(mobile.isConnected()){
+            lottieAnimationView.setVisibility(View.INVISIBLE);
+            apiService = ApiClient.getClient().create(ApiService.class);
+            Call<Data> getApiPost = apiService.getPost();
+            getApiPost.enqueue(new Callback<Data>() {
+                @Override
+                public void onResponse(Call<Data> call, Response<Data> response) {
+//                Log.e("qqq", "onResponse: "+response.body().ok);
+                    recyclerView.setAdapter(new SearchAdapter(response.body().result , getContext()));
+
+                }
+                @Override
+                public void onFailure(Call<Data> call, Throwable t) {
+                    Log.e("qqqq", "onFailure: ",t );
+
+                }
+            });
+
+        }else {
+            recyclerView.setVisibility(View.INVISIBLE);
+            lottieAnimationView.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
